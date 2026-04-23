d2966ef718e9a45f565eb7dc25d894877728a161
TarcisoVizona <tarcisovizona@gmail.com>
2026-04-23 08:17:49 -0300

    routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "../pages/Home/Home.jsx";
+import SignIn from "../pages/SignIn/SignIn.jsx";
+import Admin from "../pages/Admin/Admin.jsx";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/SignIn" element={<SignIn />}></Route>
+        <Route path="/Admin" element={<Admin />}></Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>,
