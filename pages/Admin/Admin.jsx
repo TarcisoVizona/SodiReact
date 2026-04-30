7587b797e63d4bc0e8e446e40aaca38c1c4a455d
TarcisoVizona <tarcisovizona@gmail.com>
2026-04-30 15:42:08 -0300

    gets no admin

--- pages/Admin/Admin.jsx
+++ pages/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createElement, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import sodiLogo from "./Icons/sodi2.png";
 import casa from "./Icons/casa.png";
@@ -9,32 +9,61 @@ import planilha from "./Icons/planilha.png";
 import sair from "./Icons/sair.png";
 import blackperfil from "./Icons/blackperfil.svg";
 import logout from "./Icons/logout.svg";
+const api = "http://192.168.1.6:3000";
 
 function Admin() {
+  const ordensAbertas = async () => {
+    const resposta = await fetch(`${api}/statusAberta`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    });
+    const data = await resposta.json();
+    setDados(data);
+  };
+
+  const ordensManutencao = async () => {
+    const resposta = await fetch(`${api}/statusManutencao`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    });
+    const data = await resposta.json();
+    setDados(data);
+  };
+
+  const ordensConcluidas = async () => {
+    const resposta = await fetch(`${api}/statusConcluida`, {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    });
+    const data = await resposta.json();
+    setDados(data);
+  };
+
+  const [dados, setDados] = useState([]);
+  useEffect(() => {}, [dados]);
+
   return (
-    <div className="h-screen flex">
-      <aside className="h-screen w-70 bg-adminGreen pl-6">
-        <div className="flex flex-col items-baseline justify-around h-screen">
-          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-            <img src={casa} alt="" className="w-10 h-10" />
-            <p className="text-white text-[20px]">Administração</p>
-          </div>
-          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-            <img src={planilha} alt="" className="w-10 h-10" />
-            <p className="text-white text-[20px]">Ordens de Serviço</p>
-          </div>
-          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-            <img src={maquinario} alt="" className="w-10 h-10" />
-            <p className="text-white text-[20px]">Máquinas</p>
-          </div>
-          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-            <img src={perfil} alt="" className="w-10 h-10" />
-            <p className="text-white text-[20px]">Funcionários</p>
-          </div>
-          <div className=" flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-            <img src={grafico} alt="" className="w-10 h-10" />
-            <p className="text-white text-[20px]">Histórico</p>
-          </div>
+    <div className="min-h-screen flex">
+      <aside className="flex flex-col items-baseline pl-6 justify-around min-h-full w-70 bg-adminGreen">
+        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+          <img src={casa} alt="" className="w-10 h-10" />
+          <p className="text-white text-[20px]">Administração</p>
+        </div>
+        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+          <img src={planilha} alt="" className="w-10 h-10" />
+          <p className="text-white text-[20px]">Ordens de Serviço</p>
+        </div>
+        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+          <img src={maquinario} alt="" className="w-10 h-10" />
+          <p className="text-white text-[20px]">Máquinas</p>
+        </div>
+        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+          <img src={perfil} alt="" className="w-10 h-10" />
+          <p className="text-white text-[20px]">Funcionários</p>
+        </div>
+        <div className=" flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+          <img src={grafico} alt="" className="w-10 h-10" />
+          <p className="text-white text-[20px]">Histórico</p>
         </div>
       </aside>
       <div className="flex-1">
@@ -55,21 +84,59 @@ function Admin() {
             </Link>
           </div>
         </header>
-        <div className="flex gap-13 p-6 justify-center items-xcenter">
-          <button className="cursor-pointer rounded-2xl w-60 h-30 bg-orange-400 border-0 text-[25px] text-white transition-transform duration-300 hover:scale-95">
-            Ordens abertas
-          </button>
-          <button className="cursor-pointer  rounded-2xl w-60 h-30 bg-blue-400 text-[25px] text-white transition-transform duration-300 hover:scale-95">
-            Em manutenção
-          </button>
-          <button className="cursor-pointer rounded-2xl w-60 h-30 bg-green-400 text-[25px] text-white transition-transform duration-300 hover:scale-95">
-            Concluídas
-          </button>
-          <div className="gap-10 flex flex-col justify-center items-center bg-adminGreen w-80 h-fit p-10 rounded-2xl">
-            <h1 className="text-white text-2xl">Cadastro de máquinas</h1>
-            <button className="w-60 cursor-pointer bg-white p-3 rounded-[10px]">
-              Adicionar máquinas
-            </button>
+        <div className="flex gap-13 justify-center items-start ">
+          <div className="flex-1 flex-col gap-13 p-6 justify-center items-start">
+            <div className="flex gap-6">
+              <button
+                onClick={() => ordensAbertas()}
+                className="cursor-pointer rounded-2xl w-60 h-30 bg-orange-400 border-0 text-[25px] text-white transition-transform duration-300 hover:scale-95"
+              >
+                Abertas
+              </button>
+              <button
+                onClick={() => ordensManutencao()}
+                className="cursor-pointer  rounded-2xl w-60 h-30 bg-blue-400 text-[25px] text-white transition-transform duration-300 hover:scale-95"
+              >
+                Em manutenção
+              </button>
+              <button
+                onClick={() => ordensConcluidas()}
+                className="cursor-pointer rounded-2xl w-60 h-30 bg-green-400 text-[25px] text-white transition-transform duration-300 hover:scale-95"
+              >
+                Concluídas
+              </button>
+            </div>
+            <div className="flex flex-wrap gap-6 mt-6">
+              {dados?.map((element) => {
+                return (
+                  <div className="text-[15px] w-60 h-fit shadow-2xl p-4 rounded-2xl border border-gray-100 flex flex-col gap-2">
+                    <h1 className="font-bold">Mecânico responsável:</h1>
+                    <h1>{element.nome_mecanico}</h1>
+                    <h1 className="font-bold">Data de abertura:</h1>
+                    <p>{element.data_abertura.slice(0, 10)}</p>
+                    <h1 className="font-bold">Descrição:</h1>
+                    <p>{element.descricao_problema}</p>
+                    <h1 className="font-bold">Status:</h1>
+                    <p>{element.status}</p>
+                    <h1 className="font-bold">Id da máquina:</h1>
+                    <p>{element.id_maquinas}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex gap-14">
+            <div className="gap-10 flex flex-col justify-center items-center bg-adminGreen w-80 h-fit p-10 rounded-2xl">
+              <h1 className="text-white text-2xl">Cadastro de máquinas</h1>
+              <button className="w-60 cursor-pointer bg-white p-3 rounded-[10px]">
+                Adicionar máquinas
+              </button>
+              <div>
+                <input type="text" />
+                <input type="text" />
+                <input type="text" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
