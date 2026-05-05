f212067839194dc2450c3de8d1f7487f07a8fad0
TarcisoVizona <tarcisovizona@gmail.com>
2026-05-05 16:08:36 -0300

    axios

---
+++ api/axios-config.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: "http://192.168.1.10:3000",
+  timeout: 5000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
--- pages/Admin/Admin.jsx
+++ pages/Admin/Admin.jsx
@@ -1,5 +1,5 @@
 import React, { createElement, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 import sodiLogo from "./Icons/sodi2.png";
 import casa from "./Icons/casa.png";
 import grafico from "./Icons/grafico.png";
@@ -9,17 +9,20 @@ import planilha from "./Icons/planilha.png";
 import sair from "./Icons/sair.png";
 import blackperfil from "./Icons/blackperfil.svg";
 import logout from "./Icons/logout.svg";
-const api = "http://192.168.1.6:3000";
+import axios from "axios";
+import { api } from "../../api/axios-config";
 
 function Admin() {
-  const ordensAbertas = async () => {
-    const resposta = await fetch(`${api}/statusAberta`, {
-      method: "GET",
-      headers: { "content-type": "application/json" },
-    });
-    const data = await resposta.json();
-    setDados(data);
-  };
+  const [dados, setDados] = useState([]);
+  async function ordensAbertas() {
+    try {
+      const { data, status } = await api.get("/statusAberta");
+      console.log(data);
+      setDados(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const ordensManutencao = async () => {
     const resposta = await fetch(`${api}/statusManutencao`, {
@@ -39,31 +42,34 @@ function Admin() {
     setDados(data);
   };
 
-  const [dados, setDados] = useState([]);
-  useEffect(() => {}, [dados]);
+  useEffect(() => {
+    ordensAbertas();
+  }, []);
 
   return (
     <div className="min-h-screen flex">
       <aside className="flex flex-col items-baseline pl-6 justify-around min-h-full w-70 bg-adminGreen">
-        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-          <img src={casa} alt="" className="w-10 h-10" />
-          <p className="text-white text-[20px]">Administração</p>
-        </div>
-        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-          <img src={planilha} alt="" className="w-10 h-10" />
-          <p className="text-white text-[20px]">Ordens de Serviço</p>
-        </div>
-        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-          <img src={maquinario} alt="" className="w-10 h-10" />
-          <p className="text-white text-[20px]">Máquinas</p>
-        </div>
-        <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-          <img src={perfil} alt="" className="w-10 h-10" />
-          <p className="text-white text-[20px]">Funcionários</p>
-        </div>
-        <div className=" flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
-          <img src={grafico} alt="" className="w-10 h-10" />
-          <p className="text-white text-[20px]">Histórico</p>
+        <div className="flex flex-col h-screen justify-around items-baseline-last">
+          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+            <img src={casa} alt="" className="w-10 h-10" />
+            <p className="text-white text-[20px]">Administração</p>
+          </div>
+          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+            <img src={planilha} alt="" className="w-10 h-10" />
+            <p className="text-white text-[20px]">Ordens de Serviço</p>
+          </div>
+          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+            <img src={maquinario} alt="" className="w-10 h-10" />
+            <p className="text-white text-[20px]">Máquinas</p>
+          </div>
+          <div className="flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+            <img src={perfil} alt="" className="w-10 h-10" />
+            <p className="text-white text-[20px]">Funcionários</p>
+          </div>
+          <div className=" flex justify-center items-center gap-3 cursor-pointer transition-transform duration-300 hover:scale-95">
+            <img src={grafico} alt="" className="w-10 h-10" />
+            <p className="text-white text-[20px]">Histórico</p>
+          </div>
         </div>
       </aside>
       <div className="flex-1">
@@ -84,7 +90,7 @@ function Admin() {
             </Link>
           </div>
         </header>
-        <div className="flex gap-13 justify-center items-start ">
+        <div className="flex justify-center items-start ">
           <div className="flex-1 flex-col gap-13 p-6 justify-center items-start">
             <div className="flex gap-6">
               <button
@@ -106,26 +112,24 @@ function Admin() {
                 Concluídas
               </button>
             </div>
-            <div className="flex flex-wrap gap-6 mt-6">
-              {dados?.map((element) => {
-                return (
-                  <div className="text-[15px] w-60 h-fit shadow-2xl p-4 rounded-2xl border border-gray-100 flex flex-col gap-2">
-                    <h1 className="font-bold">Mecânico responsável:</h1>
-                    <h1>{element.nome_mecanico}</h1>
-                    <h1 className="font-bold">Data de abertura:</h1>
-                    <p>{element.data_abertura.slice(0, 10)}</p>
-                    <h1 className="font-bold">Descrição:</h1>
-                    <p>{element.descricao_problema}</p>
-                    <h1 className="font-bold">Status:</h1>
-                    <p>{element.status}</p>
-                    <h1 className="font-bold">Id da máquina:</h1>
-                    <p>{element.id_maquinas}</p>
-                  </div>
-                );
+            <div id="principal" className="flex flex-wrap gap-6 mt-6">
+              {dados.map((element) => {
+                <div className="w-60 h-fit shadow-2xl p-4 rounded-2xl border border-gray-100 flex flex-col gap-2">
+                  <h1 className="font-bold">Mecânico responsável:</h1>
+                  <h1>{element.nome_mecanico}</h1>
+                  <h1 className="font-bold">Data de abertura:</h1>
+                  <p>{element.data_abertura.slice(0, 10)}</p>
+                  <h1 className="font-bold">Descrição:</h1>
+                  <p>{element.descricao_problema}</p>
+                  <h1 className="font-bold">Status:</h1>
+                  <p>{element.status}</p>
+                  <h1 className="font-bold">Id da máquina:</h1>
+                  <p>{element.id_maquinas}</p>
+                </div>;
               })}
             </div>
           </div>
-          <div className="flex gap-14">
+          <div className="flex gap-13 p-6">
             <div className="gap-10 flex flex-col justify-center items-center bg-adminGreen w-80 h-fit p-10 rounded-2xl">
               <h1 className="text-white text-2xl">Cadastro de máquinas</h1>
               <button className="w-60 cursor-pointer bg-white p-3 rounded-[10px]">
